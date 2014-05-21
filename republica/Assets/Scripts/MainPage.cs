fc716e4f9a11ea924321525fe533f497cef1fda3
Michael Mancuso <tarkenfire@tarkenfire.com>
2014-05-20 20:59:01 -0400

    Update

--- republica/Assets/Scripts/MainPage.cs
+++ republica/Assets/Scripts/MainPage.cs
@@ -3,17 +3,44 @@
 public class MainPage : OGPage
 {
 	int turnNumber = 1;
+	int score = 0;
 	private int pcPoints = 1000, cfPoints = 0, plPoints = 0;
+
+	private float partySupport = .5f;
 
 	//ui refs
 	private OGTextField pcLabel, cfLabel, plLabel;
 	private OGButton addPLButton, addCFButton, nextTurnButton;
 	private OGProgressBar supportMeter;
+	private OGTickBox camTick1, legTick1;
 
 	//button handlers
 	public void onNextTurn()
 	{
 		pcPoints += 500;
+		
+		if (camTick1.isTicked && cfPoints - 1 >= 0) {
+			--cfPoints;
+			partySupport += .1f;
+		}
+		else 
+		{
+			//TODO alert player of lack of funds
+		}
+
+		if (legTick1.isTicked && plPoints - 1 >= 0) 
+		{
+			--plPoints;
+
+		}
+		else 
+		{
+			//TODO alert player of lack of funds
+		}
+
+		partySupport -= .02f;
+
+		turnNumber++;
 		setPointTotals();
 	}
 
@@ -72,6 +99,12 @@ public class MainPage : OGPage
 
 		refObject = GameObject.Find("SupportMeter");
 		supportMeter = (OGProgressBar)refObject.GetComponent<OGProgressBar>();
+
+		refObject = GameObject.Find("cam1");
+		camTick1 = (OGTickBox)refObject.GetComponent<OGTickBox>();
+
+		refObject = GameObject.Find("leg1");
+		legTick1 = (OGTickBox)refObject.GetComponent <OGTickBox>();
 	}
 
 	private void setPointTotals()
@@ -79,6 +112,8 @@ public class MainPage : OGPage
 		pcLabel.text = "Political Capital: " + pcPoints;
 		cfLabel.text = "Camp. Funds: " + cfPoints;
 		plLabel.text = "Leverage: " + plPoints;
+
+		supportMeter.value = partySupport;
 	}
 
 }
